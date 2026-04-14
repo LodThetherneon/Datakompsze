350a724ae41d34560abc51ae4b34d91c9ef1b35e
Zoltan Kiss <141189365+LodThetherneon@users.noreply.github.com>
2026-04-14 15:23:45 +0200

    fix: print layout - ne legyen dupla html/body tag

--- src/app/policies/[id]/print/layout.tsx
+++ src/app/policies/[id]/print/layout.tsx
@@ -1,9 +1,3 @@
 export default function PrintLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="hu">
-      <body style={{ margin: 0, padding: 0, background: '#fff' }}>
-        {children}
-      </body>
-    </html>
-  )
+  return <>{children}</>
 }
